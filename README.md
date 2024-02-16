a4c939ff6679e76db05475e0b0c3fd82cb941eef
JohnWSweeney <JohnWSweeney@gmail.com>
2024-02-16 11:55:28 -0800

    Consolidated LED blink rate signals.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # zyboBlink
 
+Blinking an LED is the "Hello, world" equivalent in FPGA programming.
--- zyboBlink.srcs/sources_1/new/main.vhd
+++ zyboBlink.srcs/sources_1/new/main.vhd
@@ -32,8 +32,7 @@ end main;
 architecture Behavioral of main is
 
 signal	w_clk	: std_logic;
-signal	w_rate1	: std_logic;
-signal	w_rate2	: std_logic;
+signal	w_rate	: std_logic;
 signal	w_count	: unsigned(7 downto 0):= (others => '0');
 signal	w_state	: integer range 0 to 125000000:= 0;
 
@@ -41,20 +40,17 @@ begin
 
 w_clk 			<= sysclk;
 pmodStandard	<= std_logic_vector(w_count);
-led(0)			<= w_rate1;
-led(1)			<= not w_rate1;
-led(2)			<= w_rate2;
-led(3)			<= not w_rate2;
+led(0)			<= w_rate;
+led(1)			<= not w_rate;
+led(2)			<= w_rate;
+led(3)			<= not w_rate;
 
 process(w_clk)
 begin
 	if rising_edge(w_clk) then
 		case w_state is
-			when 1250000 =>
-				w_rate2 <= not w_rate2;
-				w_state <= w_state + 1;
 			when 12500000 =>
-				w_rate1 <= not w_rate1;
+				w_rate <= not w_rate;
 				w_count <= w_count + 1;
 				w_state <= 0;
 			when others =>
